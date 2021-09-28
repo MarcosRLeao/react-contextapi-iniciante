a91d93e95ec9e7fd31936c289e6521bef02017f5
Marcos <marcos.leao81@gmail.com>
2021-09-28 11:45:47 -0300

    first commit

---
+++ src/components/Profile.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { useAuth } from '../providers/Auth';
+
+function Profile() {
+
+    const {user} = useAuth()
+    
+    return (
+        <div style={{background: '#000', color: "#fff"}}>
+            <hr/>
+            <h1>Profile</h1>
+            <p>{user.name}</p>
+        </div>
+    );
+}
+
+export default Profile;
